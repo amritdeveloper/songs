0955cbfde7ae733f5c607648244d1affa3bf3db2
Chad Sisk <chadwyck.codeart@gmail.com>
2019-01-27 16:42:48 -0700

    Add mapStateToProps function to SongList component, pass as fist argument to connect funtion

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,15 +1,24 @@
 // can directly import the Component class
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import the connect function, notice lower case
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 // no longer need to use React.Component call
 class SongList extends Component {
-    render() {
-        return <div>SongList</div>;
-    }
+  render() {
+    console.log(this.props);
+    return <div>SongList</div>;
+  }
+}
+
+// named mapStateToProps by convention
+// takes data from state object and converts those to props in the current
+// component (in this case SongList)
+const mapStateToProps = state => {
+  // this state object will automatically become props in SongList
+  return { songs: state.songs };
 };
 
 // valid JavaScript syntax using connect on SongList
 // "A function that returns a function, calling this function"
-export default connect()(SongList);
+export default connect(mapStateToProps)(SongList);
