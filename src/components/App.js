401868cd1d961875e655aa45e6829f085f7fd4ac
Chad Sisk <chadwyck.codeart@gmail.com>
2019-01-26 17:12:37 -0700

    Add action creator selectSong functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+// selectSong is a named import, so the bracket syntax
+// But we will not be using this import, just demonstration
+// import { selectSong } from "../actions";
 
 const App = () => {
   return <div>App</div>;
