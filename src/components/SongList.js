8d7985681f2f5b1f1ce1b949c6e1df5ad334dc41
Chad Sisk <chadwyck.codeart@gmail.com>
2019-01-27 16:01:09 -0700

    Create SongList class based component

---
+++ src/components/SongList.js
@@ -0,0 +1,11 @@
+// can directly import the Component class
+import React, { Component } from 'react';
+
+// no longer need to use React.Component call
+class SongList extends Component {
+    render() {
+        return <div>SongList</div>;
+    }
+};
+
+export default SongList;
