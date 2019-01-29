753d64587880a85d6c1866b2c4984b3e62d4a47e
Chad Sisk <chadwyck.codeart@gmail.com>
2019-01-28 17:30:55 -0700

    Add action creator to the connect helper method in SongList to allow the selection of songs to update state

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,6 +2,9 @@
 import React, { Component } from "react";
 // import the connect function, notice lower case
 import { connect } from "react-redux";
+// import the action creator function: using curly braces
+// since it was originally a named export from its file
+import { selectSong } from "../actions";
 
 // no longer need to use React.Component call
 class SongList extends Component {
@@ -10,7 +13,12 @@ class SongList extends Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -27,10 +35,15 @@ class SongList extends Component {
 // takes data from state object and converts those to props in the current
 // component (in this case SongList)
 const mapStateToProps = state => {
+  console.log(state);
   // this state object will automatically become props in SongList
   return { songs: state.songs };
 };
 
 // valid JavaScript syntax using connect on SongList
 // "A function that returns a function, calling this function"
-export default connect(mapStateToProps)(SongList);
+// ES2015 Syntax do not need selectSong: selectSong key:value pair
+export default connect(
+  mapStateToProps,
+  { selectSong }
+)(SongList);
